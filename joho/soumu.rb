6d07561487313838533bb20eb0e785cd844ebb0b
INUZUKA-Katsu <inuzuka0601@gmail.com>
2022-01-12 22:33:47 +0900

    国の答申DB検索の結果画面に検索ワードのヒット部分が表示されるようにした。

--- joho/soumu.rb
+++ joho/soumu.rb
@@ -1,4 +1,5 @@
 require 'mechanize'
+require 'kconv'
 
 Net::HTTP.prepend Module.new {
   def use_ssl=(flag)
@@ -6,6 +7,12 @@ Net::HTTP.prepend Module.new {
     self.ciphers = "DEFAULT:!DH"
   end
 }
+TR=<<EOS
+          <tr class="search-result-body">
+            <td class="search-result-body-left" width="200px">該当部分<br></td>
+            <td class="search-result-body-right" width="700px"><--該当部分--><br></td>
+          </tr>
+EOS
 
 def get_soumu_search_result(search_word)
   
@@ -29,13 +36,29 @@ def get_soumu_search_result(search_word)
   form.field_with(   name: 'searchQuery'  ).value = search_word
   
   res = agent.submit(form).body.toutf8
-  
+
+  File.write("tmp/tmp.html",res)
+
+  word_array = search_word.split(/[\s　]+/)
   if res.match(/検索条件に合致する答申が見つかりませんでした。/)
     res_array = ["検索条件に合致する答申が見つかりませんでした。"]
   else
     res_array = res.gsub(/\/reportBody/,url+'\&').
                     gsub(/\/reportPointOutline/,url+'\&').
                     scan(/<table class\="search\-result".*?table>/m)
+    res_array.map! do |table|
+      honbun_url = table.match(/a href="(http.*?reportBody.*?)"/)[1]
+      #bango      = table.match(/答申\s第\s[0-9０-９]+号/)[0]
+      str        = agent.get(honbun_url).body.toutf8.gsub(/<.*?>/m,"")
+      part_str   = str.scan(/[^\n]{0,200}#{word_array.join("|")}[^\n]{0,200}\n?/).
+                     map do |s|
+                       s.gsub!(/#{word_array.join("|")}/,'<strong>\&</strong>')
+                       s.chomp
+                     end.
+                     join("<br><br>")
+      table.sub(/\n\s+<\/table>/, TR.sub(/<--該当部分-->/,part_str) + '\&')
+    end
   end
   res_array
 end
+
