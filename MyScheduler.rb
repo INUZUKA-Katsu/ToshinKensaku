771feec4de5f7e1a2fbe796d31aaf12ed1ccf2a2
INUZUKA-Katsu <inuzuka0601@gmail.com>
2024-04-27 23:12:42 +0900

    Herokuの待機時間を土日は平日より縮小し、また、平日の待機開始時刻を8時半として、Dyno使用時間の節約を図った。

--- MyScheduler.rb
+++ MyScheduler.rb
@@ -1,10 +1,17 @@
-#8時から22時までの間、10分おきに答申検索サイトをリクエストしてスリープに入るのを防止する。
-#22時から8時まではユーザーのリクエストがあるまでスリープして課金時間を節約する。
+#平日は8時30分から22時までの間、土日は12時30分から23時までの間、10分おきに答申検索サイトをリクエストしてスリープに入るのを防止する。
+#それ以外の時間帯はユーザーのリクエストがあるまでスリープして課金時間を節約する。
 require 'net/http'
 require 'uri'
 
-jst = Time.at(Time.now, in: "+09:00").hour
-if (8..22)===jst
-  #`curl http://toshin-kensaku.herokuapp.com/`
-  p Net::HTTP.get(URI.parse('http://toshin-kensaku.herokuapp.com/'))
+jst = Time.at(Time.now, in: "+09:00")
+if jst.saturday? or jst.sunday?
+  if ("12:30".."23:00")===jst.strftime("%H:%M")
+    #`curl http://toshin-kensaku.herokuapp.com/`
+    p Net::HTTP.get(URI.parse('http://toshin-kensaku.herokuapp.com/'))
+  end
+else
+  if ("08:30".."22:00")===jst.strftime("%H:%M")
+    #`curl http://toshin-kensaku.herokuapp.com/`
+    p Net::HTTP.get(URI.parse('http://toshin-kensaku.herokuapp.com/'))
+  end
 end
